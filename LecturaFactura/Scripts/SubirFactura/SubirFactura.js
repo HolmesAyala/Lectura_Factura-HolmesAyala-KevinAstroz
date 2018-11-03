64ff2d7e804e6a0d08a1d2b23f2e31c490fc67d4
Holmes Yesid Ayala Yaguara <holmesayala9@gmail.com>
2018-11-03 01:31:49 -0500

    Subida Factura base64

---
+++ LecturaFactura/Scripts/SubirFactura/SubirFactura.js
@@ -0,0 +1,47 @@
+﻿
+
+
+var FormCargarArchivo = $("#FormCargarArchivo");
+
+FormCargarArchivo.submit(
+    function (event) {
+        event.preventDefault();
+        var selecionoArchivo = 1;
+
+        var campoArchivo = document.getElementById("Archivo");
+
+        var cantidadArchivos = campoArchivo.files.length;
+
+        if (cantidadArchivos == selecionoArchivo) {
+            var archivo = campoArchivo.files[0];
+
+            /* Seria usada de la siguiente manera */
+            getBase64FromFile(archivo, function (base64) {
+                console.log(base64);
+            });
+
+            var form_data = new FormData();
+            form_data.append("Data", archivo);
+
+            console.log(archivo);
+
+            var url = "/SubirFactura/cargarFactura";
+
+            //$.post(url, form_data).done(
+            //    function (respuesta) {
+            //        console.log(respuesta);
+            //    }
+            //).fail(mostrarErrorRespuestaPost);
+        }
+    }
+);
+
+function getBase64FromFile(img, archivoBase64) {
+    let fileReader = new FileReader();
+
+    fileReader.addEventListener('load' , function (evt) {
+        archivoBase64(fileReader.result);
+    });
+
+    fileReader.readAsDataURL(img);
+}
--- Utilitaria/Clases/Respuesta.cs
+++ Utilitaria/Clases/Respuesta.cs
@@ -2,30 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
 
-namespace LecturaFactura.Controllers
+namespace LecturaFactura.App_Code
 {
-    public class SubirFacturaController : Controller
-    {
-        // GET: SubirFactura
-        public ActionResult SubirFactura()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public JsonResult cargarFactura(string Data)
-        {
-
-            return Json(Respuesta.newRespuesta(Respuesta.OK, "", Data));
-        }
-    }
-
     public class Respuesta
     {
-        public const int OK = 1;
-        public const int ERR = 1;
+        const int OK = 1;
+        const int ERR = 1;
 
         private Respuesta()
         {
